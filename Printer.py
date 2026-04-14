ce210037572d7673c92706b4d5863b2dc14d7da6
1npk412 <1npk412@gmail.com>
2026-04-14 10:41:17 -0700

    made it funnier

--- Printer.py
+++ Printer.py
@@ -1,9 +1,23 @@
 #ThePrinter
 import base64
-answerList = ['a']
-for i in range(0, len(answerList)):
-    answerList[i] = base64.b64encode(answerList[i])
-for i in range(0, len(answerList)):
-    answer = base64.b64decode(answerList[i])
-    answer = answer.decode('utf-8')
-    print(f'#{i+1} is: {(answer)}')
+
+
+def createAnswerList():
+    returnList = []
+    for i in range(1,38):
+        answer = input(f'Enter answer for {i}:')
+        returnList.append(answer.encode(encoding='utf-8'))
+    return returnList
+
+def getAnswer(answerList):
+    try:
+        for i in range(0, len(answerList)):
+            answer = base64.b64decode(answerList[i])
+            answer = answer.decode('utf-8')
+            print(f'#{i+1} is: {(answer)}')
+    except:
+        for i in range(0, len(answerList)):
+            answerList[i] = base64.b64encode(answerList[i])
+        getAnswer(answerList)
+
+getAnswer(createAnswerList())
